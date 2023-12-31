6aa468b2855928c75b1a6af1464b9184dc9cdd99
DALI <sika.dali@telecomnancy.net>
2023-12-31 12:41:33 +0100

    begin API impl

--- weather.js
+++ weather.js
@@ -1,3 +1,22 @@
-let url = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
-let cityName = "paris";
-let apikey = "";
+import { weatherApiKey } from "./config.js";
+
+let cityNameInput = document.getElementById("city");
+let backButton = document.getElementById("back");
+let btnSearch = document.querySelector(".weather .enter-city button");
+
+backButton.style.display = "none";
+
+btnSearch.addEventListener("click", () => {
+     let inputWord = cityNameInput.value;
+     let url = `https://api.openweathermap.org/data/2.5/weather?q={${inputWord}}&appid={${weatherApiKey}}`;
+
+     console.log(url);
+     fetch(url)
+          .then((response) => response.json())
+          .then((data) => {
+               console.log(data);
+          })
+          .catch((error) => {
+               alert(error.message);
+          });
+});
